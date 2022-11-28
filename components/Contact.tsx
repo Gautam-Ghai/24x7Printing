4a813ce3365a65a592a80bf5f9cd0f3757286fe7
Gautam <gautamghai99@gmail.com>
2022-11-27 19:47:03 -0800

    updating email link

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -26,8 +26,8 @@ function Contact() {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                 </svg>
               </div>
-              <Link href={"mailto:print2go247@gmail.com"} >
-                <h3 className='cursor-pointer ml-4 font-open text-lg md:text-2xl font-medium text-textColor py-4'><a href="https://form.jotform.com/223258749959274">printtogo247@gmail.com</a></h3>
+              <Link href={"mailto:printtogo247@gmail.com"} >
+                <h3 className='cursor-pointer ml-4 font-open text-lg md:text-2xl font-medium text-textColor py-4'>printtogo247@gmail.com</h3>
               </Link>
             </div>
             <div className='flex m-4'>
